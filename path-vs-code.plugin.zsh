945c6316b23e1359e4105293b412ebdbd33845c0
Alexandru Rosianu <1418870+aluxian@users.noreply.github.com>
2025-09-07 14:23:12 +0300

    only if installed

--- path-vs-code.plugin.zsh
+++ path-vs-code.plugin.zsh
@@ -1,3 +1,3 @@
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [[ "$OSTYPE" == darwin* ]] && [[ -d "/Applications/Visual Studio Code.app" ]]; then
   export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
 fi
